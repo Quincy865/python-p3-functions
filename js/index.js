9a4aa192afe04a7bccf14bdf6e15ce3a283ef64c
Quincy Michael <michaelkirima865@gmail.com>
2024-11-28 10:23:41 +0300

    Functions in Python

--- js/index.js
+++ js/index.js
@@ -50,7 +50,9 @@ function add(num1, num2) {
   => null
 */
 function halve(number) {
-  if (typeof number !== "number") return null;
-
+  if (typeof number !== "number") {
+    console.log("Input must be a number.");
+    return null;
+  }
   return number / 2;
 }
